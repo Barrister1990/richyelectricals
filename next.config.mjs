b616cc053e0515e2c4b063ac7161f5481ca4fa63
Barrister <charlesawuku2010@gmail.com>
2025-04-27 16:54:04 -0500

    Initial commit after cleaning

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+ 
   images: {
     unoptimized: true, // Disable image optimization for static export
   },
